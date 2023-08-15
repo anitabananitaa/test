c2f95b0036d39ab21111e0a6944bea06dcd180f3
ana <anaibacache19@gmail.com>
2023-08-15 20:51:07 -0300

    hice un nuevo html

--- tp prog 2/main.js
+++ tp prog 2/main.js
@@ -1,91 +1,11 @@
-function mostrarDatos(){
-	const Usuario = document.getElementById('usuario');
-	const Contraseña = document.getElementById('contraseña');
+function MostrarDatos(){
+	var dato1 = document.getElementById("input1").value;
+	var dato2 = document.getElementById("input2").value;
+
+	console.log("Useraname:", dato1);
+	console.log("Password", dato2);
 }
 
-
-let aaa=[]
-function escribir(materias){
-	const divContenedorM = document.getElementById('contenedorM');
-	limpiar();
-	materias.forEach((materia, index) =>{
-
-	
-		const promedio = materia.notas.reduce((acu, calif ) => acu + calif, 0) /materia.notas.length;
-
-		console.log(promedio) 
-
-		const materiasHTML = `<div class="Cuadradito">
-			<span onclick="eliminar(${index})" class="EliminarMateria"> x </span>
-			<span>${materia.nombre}</span>
-			<span>${materia.docente}, ${materia.horas}</span>
-			<span>cod: ${materia.codigo} - ${materia.notas}</span>
-			<span>prom: ${promedio} </span>
-			</div>`
-
-	divContenedorM.innerHTML += materiasHTML;
-	} )
-}
-
-function agregarN(nota){
-	const divContenedorNotas = document.getElementById('ContenedorNotas');
-	const notaHTML = `<input type="number" name="nota" class="num">`
-
-	divContenedorNotas.innerHTML += notaHTML;
-}
-	
-function eliminar(index){
-	materias.splice(index, 1);
-	escribir(materias);
-
-}
-function limpiar(){
-	const divContenedorM = document.getElementById('contenedorM');
-	divContenedorM.innerHTML = "";
-
-}
-
-function eliminarTodos(){
-	materias = [];
-	limpiar();
-}
-
-function Ordenar(){
-	materias.sort((a, b) => a.nombre.toUpperCase() > b.nombre.toUpperCase());
-	escribir(materias);
-}
-
-
-function alerta(){
-	const nombre = document.getElementById('materia').value;
-	const docente = document.getElementById('docente').value;
-	const horas = document.getElementById('horas').value;
-	const codigo = document.getElementById('codigo').value;
-	const notasHTML = [...document.getElementsByName('nota')];
-	
-	const notas = notasHTML.map(nota => parseInt(nota.value));
-
-	const materia = {
-		nombre,
-		docente,
-		horas,
-		codigo,
-		notas
-		
-		
-	}
-console.log(materia)
-
-	materias.push(materia)
-
- 	escribir(materias);
-
-
-}
-
-function moverCur(event, campoSiguiente) {
-	if (event.keyCode === 13 ) {
-		document.getElementById(campoSiguiente).focus();
-		event.preventDefault();
-	}
+function redireccionar(){
+	window.location.href ='file:///C:/Users/ibacachea/Documents/GitHub/test/tp%20prog%202/crear%20cuenta/cuenta.html';
 }
